f9e413ce7f1239f5c5f75d80eeb19a26db8830ef
Yaxin Yuan <jack.yuan@emmetros.com>
2017-11-25 19:16:19 -0500

    update

---
+++ NotUrgent/View/TargetListCell.swift
@@ -0,0 +1,13 @@
+//
+//  TargetListCell.swift
+//  NotUrgent
+//
+//  Created by Yaxin Yuan on 2017-11-25.
+//  Copyright © 2017 Yaxin Yuan. All rights reserved.
+//
+
+import UIKit
+
+class TargetListCell: UITableViewCell {
+
+}
