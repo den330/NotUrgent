0e4d3e1d986775a79dafa06eeb51412c49834403
Yaxin Yuan <jack.yuan@emmetros.com>
2017-11-25 19:58:49 -0500

    update

---
+++ NotUrgent/ViewModel/TargetListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TargetListViewModel.swift
+//  NotUrgent
+//
+//  Created by Yaxin Yuan on 2017-11-25.
+//  Copyright © 2017 Yaxin Yuan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+
+class TargetListViewModel: NSObject{
+//
+//    func getNumOfSections() -> Int{
+//
+//    }
+
+}
